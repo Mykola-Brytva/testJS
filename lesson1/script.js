8de77a365ee22a046e27d1c0d356caf8a6f0d0c3
mozk <krutto@mail.ru>
2019-02-10 01:59:23 +0300

    My first change

---
+++ lesson1/script.js
@@ -0,0 +1,59 @@
+'use strict';
+
+let a = 5;
+console.log(a);
+
+var number = 5;
+var string = 'Hellooo';
+var sym = Symbol();
+var bool = true;
+null;
+undefined;
+var obj = {};
+
+console.log(4/0);
+console.log('string '+9);
+
+let something;
+
+console.log(something);
+
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+console.log(persone["isMarried"]);
+
+let arr = ['plum.png','orange.jpg','apple.bmp'];
+
+console.log(arr[0]);
+
+//alert('Holla');
+
+//let answer = confirm("Are you still here?");
+
+//console.log(answer);
+
+//let answer = +prompt("Are you allready 18?", "YES");
+
+//console.log(typeof(answer));
+//console.log(++answer);
+
+/*let incr = 10,
+    decr = 10;
+
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);*/
+
+console.log("2"==2);
+
+let isChecked = true,
+    isClosed = false;
+
+console.log(isChecked || isClosed);
+
